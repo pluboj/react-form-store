b96f42f592d8e50f3eccd7766615676d7b81353b
plubojacky <plubojacky@email.com>
2017-08-04 15:01:24 -0700

    Basic project setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
+  state = {
+    step: 1
+  }
+
+  render() {
+    switch (this.state.step) {
+      case 1:
+        return <ProductList />;
+      case 2:
+        return <ShippingDetails />;
+      case 3:
+        return <DeliveryInformation />
+      default:
+        return <ProductList />;
+    }
+  }
+}
+
+class ProductList extends Component {
   render() {
     return (
-      <div className="App">
-       
-      </div>
-    );
+      <h1>
+        Choose from wide variety of our products.
+      </h1>
+    )
+  }
+}
+
+class ShippingDetails extends Component {
+  render() {
+    return (
+      <h1>
+        Enter your shipping information.
+      </h1>
+    )
+  }
+}
+
+class DeliveryInformation extends Component {
+  render() {
+    return (
+      <h1>
+        Choose your delivery option.
+      </h1>
+    )
   }
 }
 
