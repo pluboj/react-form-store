adf1aa2db37d14a57b08c2f36633419bcbc90ac3
plubojacky <plubojacky@email.com>
2017-08-04 17:48:39 -0700

    Update layout

--- src/App.js
+++ src/App.js
@@ -22,11 +22,48 @@ class App extends Component {
 }
 
 class ProductList extends Component {
+  state = {
+    items: [
+      {id: '10200', name: 'item 1', color: 'red'},
+      {id: '50250', name: 'item 2', color: 'black'},
+      {id: '00601', name: 'item 3', color: 'grey'},
+      {id: '78009', name: 'item 4', color: 'yellow'},
+    ]
+  }
+
+  renderItem(item) {
+    const clrStyle = {
+      background: 'red'
+    }
+
+    return (
+      <div className="checkbox">
+          <label className="well">
+            <input type="checkbox" className="big-check"/> 
+            <div class="div-text">
+            #{item.id} 
+             -- {item.name}
+             -- color: <div className="clr" clr={item.color} 
+              style={{background: item.color}}>{item.color}</div>
+              </div>
+          </label>
+      </div>
+    )
+  }
   render() {
     return (
-      <h1>
-        Choose from wide variety of our products.
-      </h1>
+      <div>
+        <h3>
+          Choose from wide variety of our products.
+        </h3>
+        <form>
+          {this.state.items.map((item) => {
+            return this.renderItem(item);
+            })
+          }
+          <input type="submit" className="btn btn-success" />
+        </form>
+      </div>
     )
   }
 }
